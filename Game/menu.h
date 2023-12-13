04f9693385c4ec0d976a23a21e2a0a4922a07638
Maxime.C <115615422+Lexilios@users.noreply.github.com>
2023-12-13 20:04:54 +0100

    Add files via upload

---
+++ Game/menu.h
@@ -0,0 +1,25 @@
+// Menu.h
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Menu {
+public:
+    Menu();
+    ~Menu();  // Ajoutez la déclaration du destructeur
+
+    void Show(sf::RenderWindow& window);
+    bool PlayState = false;
+
+private:
+    sf::RectangleShape playButton;
+    sf::RectangleShape optionsButton;
+    sf::RectangleShape quitButton;
+    sf::Font font;
+    sf::Text playText;
+    sf::Text optionsText;
+    sf::Text quitText;
+    sf::Texture backgroundTexture;
+    sf::Text titleText;
+
+    void HandleEvents(sf::Event& event, sf::RenderWindow& window);
+};
